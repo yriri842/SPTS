28774f6ae31330ab786e4d39e448f491e0d44bbc
yriri842 <coraanddust@gmail.com>
2026-07-05 08:34:53 +0300

    Update util_tab.lua

--- SPTS/ui/util_tab.lua
+++ SPTS/ui/util_tab.lua
@@ -1,10 +1,8 @@
--- Utilities tab: instant respawn toggle and manual respawn button.
-
 local Tabs    = _G.Tabs
 local Toggles = _G.Toggles
+local Rayfield = _G.Rayfield
 
 Tabs.Util:CreateSection("Respawn")
-
 Toggles["IR"] = Tabs.Util:CreateToggle({
     Name         = "Instant Respawn",
     CurrentValue = false,
@@ -13,8 +11,25 @@ Toggles["IR"] = Tabs.Util:CreateToggle({
 })
 
 Tabs.Util:CreateSection("Manual")
-
 Tabs.Util:CreateButton({
     Name     = "Respawn Here",
     Callback = function() _G.doRespawn() end,
+})
+
+Tabs.Util:CreateSection("Script")
+Tabs.Util:CreateButton({
+    Name     = "Unload Script",
+    Callback = function()
+        Rayfield:Notify({
+            Title    = "SPTS",
+            Content  = "Unloading everything...",
+            Duration = 2,
+            Image    = "power",
+        })
+        task.spawn(function()
+            if _G.SPTS_Unload then
+                _G.SPTS_Unload()
+            end
+        end)
+    end,
 })
