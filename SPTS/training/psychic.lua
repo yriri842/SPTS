e8bbf8dde20bd0ba9e12d19290e14c7e88788598
yriri842 <coraanddust@gmail.com>
2026-07-05 12:17:33 +0300

    Update psychic.lua

--- SPTS/training/psychic.lua
+++ SPTS/training/psychic.lua
@@ -129,7 +129,7 @@ task.spawn(function()
                         root.CFrame = CFrame.new(target)
                         _G.ppTeleported = true
                         local waited = 0
-                        while waited < 4 do
+                        while waited < 5.5 do
                             if not _G.Settings.PsychicPower then
                                 _G.unequipAllTools()
                                 _G.ppTeleported = false
