35bad28e4165ba992df77e54f65f5ecaf15f77f2
yriri842 <coraanddust@gmail.com>
2026-07-05 08:29:04 +0300

    Update mobility.lua

--- SPTS/training/mobility.lua
+++ SPTS/training/mobility.lua
@@ -1,12 +1,6 @@
--- Movement Speed and Jump Force training loops.
--- Both stats fire server remotes on a timer. Weight equipping is handled
--- by the Sath farm phase logic in sath/farm.lua.
-
 local Remote = _G.Remote
-
--- MS fires every 0.25 s.
 task.spawn(function()
-    while true do
+    while _G.SPTS_ALIVE ~= false do
         if _G.Settings.MovementSpeed and not _G.sathAutofarmBlocked() then
             Remote:FireServer({ [1] = "Add_MS_Request" })
         end
@@ -14,9 +8,8 @@ task.spawn(function()
     end
 end)
 
--- JF fires every 0.25 s.
 task.spawn(function()
-    while true do
+    while _G.SPTS_ALIVE ~= false do
         if _G.Settings.JumpForce and not _G.sathAutofarmBlocked() then
             Remote:FireServer({ [1] = "Add_JF_Request" })
         end
@@ -24,9 +17,8 @@ task.spawn(function()
     end
 end)
 
--- Re-equips the active weight tier every 3 s to keep it from falling off.
 task.spawn(function()
-    while true do
+    while _G.SPTS_ALIVE ~= false do
         local w = _G.Settings.ActiveWeight
         if w > 0 then
             Remote:FireServer({ [1] = "EquipWeight_Request", [2] = w })
